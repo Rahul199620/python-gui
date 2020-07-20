2406650bf3515fdd93e2595b14f4d9f78e7f2b5b
Rahul <cse.rahul01@gmail.com>
2020-07-20 18:50:42 +0530

    added combo box functinality

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ def press():
     user=name_var.get()
     email=email_var.get()
     age=age_var.get()
-    print(f'{user} is{ age},{email}')
+    gen=gen_var.get()
+
+    print(f'{user} is{ age},{email},{gen}')
 
 name= ttk.Label(soft,text='enter your name').grid(row=0,column=0,sticky=tk.W)
 email= ttk.Label(soft,text='enter your email').grid(row=1,column=0,sticky=tk.W)
-name= ttk.Label(soft,text='enter age').grid(row=2,column=0,sticky=tk.W)
+age= ttk.Label(soft,text='enter age').grid(row=2,column=0,sticky=tk.W)
+gender= ttk.Label(soft,text='gender').grid(row=3,column=0,sticky=tk.W)
 #entery box
 name_var=tk.StringVar()
 name_box=ttk.Entry(soft,width=16,textvariable=name_var).grid(row=0,column=1)
@@ -22,5 +25,11 @@ email_box=ttk.Entry(soft,width=16,textvariable=email_var).grid(row=1,column=1)
 age_var=tk.StringVar()
 age_box=ttk.Entry(soft,width=16,textvariable=age_var).grid(row=2,column=1)
 
-button = ttk.Button(soft,text='submit',command=press).grid(row=3,column=0)
+gen_var=tk.StringVar()
+gender=ttk.Combobox(soft,width=16,textvariable=gen_var)
+gender['values']=('Male','Female','other')
+gender.grid(row=3,column=1)
+button = ttk.Button(soft,text='submit',command=press).grid(row=4,column=0)
+
+
 soft.mainloop()
