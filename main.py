77a09dc57804136685e6a5f67e6d50f226c7824a
Rahul <cse.rahul01@gmail.com>
2020-07-20 17:22:53 +0530

    adding 3 labels to window

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ import tkinter as tk
 from tkinter import ttk
 soft= tk.Tk() 
 #create label
-name= ttk.Label(soft,text='enter your name').grid()
+name= ttk.Label(soft,text='enter your name').grid(row=0,column=0,sticky=tk.W)
+email= ttk.Label(soft,text='enter your email').grid(row=1,column=0,sticky=tk.W)
+name= ttk.Label(soft,text='enter age').grid(row=2,column=0,sticky=tk.W)
 soft.mainloop()
