812fc21aabee56017629b9157cfae89704483a3b
Rahul <cse.rahul01@gmail.com>
2020-07-20 17:58:31 +0530

    added entry boxex

--- main.py
+++ main.py
@@ -6,4 +6,14 @@ soft= tk.Tk()
 name= ttk.Label(soft,text='enter your name').grid(row=0,column=0,sticky=tk.W)
 email= ttk.Label(soft,text='enter your email').grid(row=1,column=0,sticky=tk.W)
 name= ttk.Label(soft,text='enter age').grid(row=2,column=0,sticky=tk.W)
+#entery box
+name_var=tk.StringVar()
+name_box=ttk.Entry(soft,width=16,textvariable=name_var).grid(row=0,column=1)
+
+email_var=tk.StringVar()
+email_box=ttk.Entry(soft,width=16,textvariable=email_var).grid(row=1,column=1)
+
+age_var=tk.StringVar()
+age_box=ttk.Entry(soft,width=16,textvariable=age_var).grid(row=2,column=1)
 soft.mainloop()
+
