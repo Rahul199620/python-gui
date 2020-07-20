df651f0f53f54adb19150a5dfc0f0bafb573c00e
Rahul <cse.rahul01@gmail.com>
2020-07-20 17:06:11 +0530

    adding labels to window

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-import tkinter 
-soft= tkinter.Tk() 
-soft.mainloop() 
+#import the useful libraries 
+import tkinter as tk
+from tkinter import ttk
+soft= tk.Tk() 
+#create label
+name= ttk.Label(soft,text='enter your name').grid()
+soft.mainloop()
