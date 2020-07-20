aadaa8cf768a8741ad7f6ffa2d672e0748e1a7c7
Rahul <cse.rahul01@gmail.com>
2020-07-20 18:24:26 +0530

    print value of form on terminal

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import tkinter as tk
 from tkinter import ttk
 soft= tk.Tk() 
 #create label
+def press():
+    user=name_var.get()
+    email=email_var.get()
+    age=age_var.get()
+    print(f'{user} is{ age},{email}')
+
 name= ttk.Label(soft,text='enter your name').grid(row=0,column=0,sticky=tk.W)
 email= ttk.Label(soft,text='enter your email').grid(row=1,column=0,sticky=tk.W)
 name= ttk.Label(soft,text='enter age').grid(row=2,column=0,sticky=tk.W)
@@ -15,5 +21,6 @@ email_box=ttk.Entry(soft,width=16,textvariable=email_var).grid(row=1,column=1)
 
 age_var=tk.StringVar()
 age_box=ttk.Entry(soft,width=16,textvariable=age_var).grid(row=2,column=1)
-soft.mainloop()
 
+button = ttk.Button(soft,text='submit',command=press).grid(row=3,column=0)
+soft.mainloop()
